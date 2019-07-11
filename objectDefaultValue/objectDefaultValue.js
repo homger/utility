585dfa21ab7806ff7adf627cdf1c85a31da0cd9c
Guerchôm Doré <d.homger@gmail.com>
2019-07-11 11:51:44 +0200

    now check defaultObject

--- objectDefaultValue/objectDefaultValue.js
+++ objectDefaultValue/objectDefaultValue.js
@@ -6,6 +6,9 @@
 
 */
 function objectDefaultValue(objectToCheck, defaultObject){
+  if(typeof defaultObject !== "object"){
+    throw new Error("(typeof defaultObject !== 'object') == true");
+  }
   let keyArray = Object.keys(defaultObject);
   keyArray.forEach(function(key){
     if(typeof objectToCheck[key] !== typeof defaultObject[key]){
