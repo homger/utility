2ecf568c53c80039fbc835d462a762f38cf16372
Guerchôm Doré <d.homger@gmail.com>
2019-08-17 14:01:37 +0200

    added hasClass function wil eed to modifi the others

--- classFunction/gd_classFunction.js
+++ classFunction/gd_classFunction.js
@@ -1,5 +1,15 @@
 //FOR IE
 
+function hasClass(element, _class){
+  let index = element.className.indexOf(_class);
+
+  if(index > -1){
+    
+    return checkBorder(element.className, index, _class.length);
+  }
+  return false;
+}
+
 function removeClass(element, _class){
   let index = element.className.indexOf(_class);
 
@@ -25,4 +35,13 @@ function toggleClass(element, _class){
     return;
   }
   element.className = element.className + " " + _class;
+}
+
+function checkBorder(string, index, length){
+  let m_length = length - 1;
+  if(index > 0 && string[index - 1] !== " ")
+    return false;
+  if(string.length > index + length && string[index + length] !== " ")
+    return false;
+  return true;
 }
